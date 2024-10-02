24330b025eb1a547838ed5e9c3e9f5eacd762be9
huzaifawaleed <huzaifainaam225@gmail.com>
2024-10-02 11:48:11 +0500

    Task-1

---
+++ script.js
@@ -0,0 +1,13 @@
+       
+ function openModal(imageSrc) {
+    const modal = document.getElementById('imageModal');
+    const modalImage = document.getElementById('modalImage');
+    modal.style.display = 'flex'; 
+    modalImage.src = imageSrc;  
+  }
+  
+  
+  function closeModal() {
+    const modal = document.getElementById('imageModal');
+    modal.style.display = 'none';  
+  }
